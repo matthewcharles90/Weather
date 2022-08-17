5f9651b7ee2eddd78b73e546bdb9aada181eb08c
Matthew Charles <matthewcharles90@gmail.com>
2022-08-17 01:23:38 -0400

    Update

---
+++ weather-app/src/App.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import axios from 'axios'
+import "./index.css"
+
+function App() {
+
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=2e2955a6d625718e24ca7081ea908f28`
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+
+    }
+
+  }
+
+  return (
+    <div className="App">
+      <div className="search">
+        <input
+          type="text"
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location' />
+      </div>
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>{data.name}</p>
+          </div>
+          <div className="temp">
+            {data.main ? <h1>{data.main.temp.toFixed()} F </h1> : null}
+
+          </div>
+          <div className="description">
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
+
+          </div>
+        </div>
+
+        {/* Only display when you enter a value in search */}
+        {data.name !== undefined &&
+
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p className='bold'>{data.main.feels_like.toFixed()} F </p> : null}
+              <p>Feels Like</p>
+            </div>
+            <div className="humidity">
+              {data.main ? <p className='bold'>{data.main.humidity.toFixed()} %</p> : null}
+              <p>Humiduty</p>
+            </div>
+            <div className="wind">
+              {data.wind ? <p className='bold'>{data.wind.speed.toFixed()} MPH </p> : null}
+              <p>Wind Speed</p>
+            </div>
+          </div>
+
+        }
+
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
